dd0f56268f15dd5c2b1d5b9a4fa534d448b45f6b
Steven Bister (University of Birmingham Conferences and Events) <s.bister@bham.ac.uk>
2020-04-25 11:43:37 +0100

    Added inital functions to grab the required letters from password

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('it works!')
+const app = document.querySelector('#app');
+const form = document.querySelector('#password-checker');
+const { password, letters } = form;
+
+let pwdLength;
+
+// Count the number of letters in the password
+// We don't want users to be able to ask for more letters than available
+const countLetters = input => input.value.length;
+password.addEventListener('keyup', () => (pwdLength = countLetters(password)));
+
+// Turn string into an array, broken on the comma
+const strToArr = str => str.replace(/\s/g, '').split(',');
+
+// Check the positions entered
+const getLetters = (word, position) => position.map(i => word.split('')[i]);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  
+  const div = document.createElement('div');
+  div.classList.add('output');
+  div.innerHTML = `<p>${getLetters(password.value, strToArr(letters.value))}</p>`;
+  app.appendChild(div);
+
+});
