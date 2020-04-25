33e13b9f8486c49d9415e7c0e80a2605823982f6
Steven Bister (University of Birmingham Conferences and Events) <s.bister@bham.ac.uk>
2020-04-25 13:09:55 +0100

    Created function to check the length of the password agains the number's in the letters input

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 import strToArr from './handlers/handleStringToArray';
 import getLetters from './handlers/handleGetLetters';
+import { countLetters, checkLetters } from './handlers/handleCheckPasswordLength';
 
 const app = document.querySelector('#app');
 const form = document.querySelector('#password-checker');
 const { password, letters } = form;
-
 let pwdLength;
 
 // Count the number of letters in the password
-// We don't want users to be able to ask for more letters than available
-const countLetters = input => input.value.length;
-password.addEventListener('keyup', () => (pwdLength = countLetters(password)));
+password.addEventListener('keyup', () => pwdLength = countLetters(password));
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  
-  console.log(getLetters(password.value))
- 
+  checkLetters(letters, pwdLength);
+
   const div = document.createElement('div');
   div.classList.add('output');
   div.innerHTML = `<p>${getLetters(password.value, strToArr(letters.value))}</p>`;
