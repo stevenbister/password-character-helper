998aca92314b2c54e10558f69ea4edecb8772be9
Steven Bister (University of Birmingham Conferences and Events) <s.bister@bham.ac.uk>
2020-04-25 12:33:20 +0100

    Check if there is only one number entered

--- handlers/handleStringToArray.js
+++ handlers/handleStringToArray.js
@@ -1,12 +1,18 @@
 // Turn string into an array, broken on the comma
 const strToArr = str => {
-  // Check the string contains a comma to split on
+  // Init an empty array ready to store the letters
+  let arr = [];
+
+  // If the string contains a comma split the sting into an array
+  // Else push the number into an array on it's own
   const regex = /,/;
+  if (regex.test(str)) {
+    return str.replace(/\s/g, '').split(',');
+  }
 
-  // Early return if comma doesn't exist
-  if (!regex.test(str)) return; 
+  arr.push(str);
 
-  return str.replace(/\s/g, '').split(',')
+  return arr;
 };
 
 export default strToArr;
