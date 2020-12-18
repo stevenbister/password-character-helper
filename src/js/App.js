e2e1c1e34dab55c64ac7b2b675b3148c3e8d73bf
Steven Bister <stevebister@gmail.com>
2020-12-18 16:24:32 +0000

    Feat: created functions to watch input and push into an array
    This will allow us to use the array later on when we want to transform the values

--- src/js/App.js
+++ src/js/App.js
@@ -1,1 +1,37 @@
-console.log('test');
+const passwordContainer = document.getElementById('passwordContainer');
+
+function pushInputTextToArray(e) {
+  // Get the contents of the div
+  const innertext = e.target.innerHTML.trim();
+
+  // Spread contents into the array
+  return array = [...innertext]
+}
+
+function pushHiddenCharactersToArray(array) {
+  if (!Array.isArray(array)) throw new TypeError;
+
+  // Return characters as *'s so they're hidden to the user
+  return array.map(character => character.innerText = '*');
+}
+
+function wrapCharacters(character) {
+  if (!typeof character == 'string') throw new TypeError;
+
+  const trimmedCharacters = character.trim();
+
+  return trimmedCharacters.split('').map(character => `<span>${character}</span>`).join('');
+}
+
+
+passwordContainer.addEventListener('input', function(e) {
+  const charactersArray = pushInputTextToArray(e);
+  const hiddenCharactersArray = pushHiddenCharactersToArray(charactersArray);
+
+  console.log(charactersArray);
+
+  // TODO: Turn this into the hidden characters
+  return e.target.innerHTML = charactersArray.join('');
+
+});
+
