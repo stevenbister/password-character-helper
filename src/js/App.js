3e700879f1d2f66946672f12c8968f2ecfec0a3e
Steven Bister <stevebister@gmail.com>
2021-01-08 16:08:35 +0000

    Feat: Added function to add results to the output based on checkbox checked state

--- src/js/App.js
+++ src/js/App.js
@@ -5,7 +5,10 @@ import createCheckbox from './createCheckbox';
 let characters = [];
 
 const passwordContainer = document.getElementById('passwordContainer');
-const checkboxContainer = document.querySelector('#characterPicker fieldset > .checkboxes');
+const checkboxContainer = document.querySelector(
+  '#characterPicker fieldset > .checkboxes'
+);
+const output = document.getElementById('output');
 
 passwordContainer.addEventListener('input', (e) => {
   // Clear the checkbox wrapper so we start with a blank slate on each input.
@@ -19,7 +22,37 @@ passwordContainer.addEventListener('input', (e) => {
       array: characters,
       container: checkboxContainer,
     });
-  }
+  };
 
   return appendCheckboxes();
+});
+
+/**
+ * Use event delegation to handle the event listener.
+ * Because the checkboxes are dynamically created we can't target them with the element.addEventListener
+ * unless we add a new event listener each time we create one.
+ */
+document.addEventListener('change', (e) => {
+  if (e.target.matches('input[type="checkbox"]')) {
+    const id = e.target.id;
+
+    // Compare array index with checkbox ID
+    const showCharacters = () => {
+
+      return characters.forEach((character, i) => {
+        if (id == i + 1) {
+          if (e.target.checked) {
+            output.innerHTML += `<p>${character}</p>`;
+            console.log({character, i});
+          } else {
+            // TODO: Not quite working - this is checking agains the i of the loop rather than the i of the nodelist
+              output.removeChild(output.childNodes[i]);
+          }
+        }
+      });
+
+    };
+
+    return showCharacters();
+  }
 });
