0cbd6b3c63181ca9e0f0d549e7c137d9ef029bdc
Steven Bister <stevebister@gmail.com>
2021-01-08 12:45:50 +0000

    Refactor: as we're using the += we don't need to append the label seperately.
    This also fixes a bug where all inputs were getting wrapped in the same label.

--- src/js/createCheckbox.js
+++ src/js/createCheckbox.js
@@ -2,13 +2,9 @@ function createCheckbox({ array, container }) {
   if (!Array.isArray(array)) throw new TypeError();
   if (!container) throw new Error('No container for your checkboxes defined');
 
-  const label = document.createElement('label');
-
-  return array.forEach((item, i) => {
-    label.innerHTML += `${i+1} <input type="checkbox" name="${i+1}" id="${i+1}">`;
-
-    return container.append(label);
-  });
+  return array.forEach((item, i) => (
+    container.innerHTML += `<label>${i+1} <input type="checkbox" name="${i+1}" id="${i+1}"></label>`)
+  );
 }
 
 export default createCheckbox;
