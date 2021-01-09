85b6d36c8120f613d16888487344d2a632a86d52
Steven Bister <stevebister@gmail.com>
2021-01-09 12:44:22 +0000

    Feat: Added function to add character containers
    This will help us fix the posiiton of the characters

---
+++ src/js/createElements.js
@@ -0,0 +1,20 @@
+function createCheckbox({ array, container }) {
+  if (!Array.isArray(array)) throw new TypeError();
+  if (!container) throw new Error('No container for your checkboxes defined');
+
+  return array.forEach((item, i) => (
+    container.innerHTML += `<label>${i+1} <input type="checkbox" name="${i}" id="${i}"></label>`)
+  );
+}
+
+function createCharacterContainer({ array, container }) {
+  if (!Array.isArray(array)) throw new TypeError();
+  if (!container) throw new Error('No container for your checkboxes defined');
+
+  return array.forEach((item, i) => (
+    // TODO: Remove the ${item}, only here for debugging for now
+    container.innerHTML += `<span data-index="${i}"></span>`
+  ))
+}
+
+export {createCheckbox, createCharacterContainer};
