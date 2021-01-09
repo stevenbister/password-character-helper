74b85f16e62c293be3db21525ff325afb5deafeb
Steven Bister <stevebister@gmail.com>
2021-01-09 15:22:50 +0000

    Feat: added class on checked value

--- src/js/App.js
+++ src/js/App.js
@@ -44,6 +44,7 @@ passwordContainer.addEventListener('input', (e) => {
 document.addEventListener('change', (e) => {
   if (e.target.matches('input[type="checkbox"]')) {
     const id = e.target.id;
+    const checkboxParent = e.target.parentElement;
 
     // Compare array index with checkbox ID
     const showCharacters = () => {
@@ -51,8 +52,12 @@ document.addEventListener('change', (e) => {
         if (id == i) {
           if (e.target.checked) {
             wrapper.innerText = characters[i];
+
+            checkboxParent.classList.add('checked');
           } else {
             if (wrapper.dataset.index == [i]) wrapper.innerText = '';
+
+            checkboxParent.classList.remove('checked');
           }
         }
       });
