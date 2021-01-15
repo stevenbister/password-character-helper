449b18797d1a26fa51d898dec981e69711dfc3ac
Steven Bister <stevebister@gmail.com>
2021-01-15 14:53:24 +0000

    Test: Added initial test files (#6)

---
+++ cypress/integration/main_spec.js
@@ -0,0 +1,46 @@
+function createPwd(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+const passwordLength = 12;
+const password = createPwd(passwordLength);
+
+describe('Opens the home page', () => {
+  it('successfully loads', () => {
+    cy.visit('/');
+  });
+})
+
+describe('Focuses the password input and enters a password', () => {
+  const passwordInput = 'input[type=password]';
+
+  it('successfully focussed', () => {
+    cy.get(passwordInput).focus();
+  });
+
+  it('enters a password', () => {
+    cy.get(passwordInput).type(password);
+  });
+})
+
+describe('Selects all of characters to show', () => {
+  const checkboxes = 'input[type=checkbox]';
+
+  it('selects the checkboxes', () => {
+    cy.get(checkboxes).each((el) => {
+      // wrap this element so we can
+      // use cypress commands on it
+      cy.wrap(el).click();
+    });
+  });
+
+  it('checks the output length matches the password', () => {
+    cy.get('#output').children().should('have.length', passwordLength)
+  });
+})
