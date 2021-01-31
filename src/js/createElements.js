4985613cb7b882c4846e853ed24fdab5afd1c2b4
Steven Bister <stevebister@gmail.com>
2021-01-31 11:56:34 +0000

    Fix: Added focus styles to checkboxes

--- src/js/createElements.js
+++ src/js/createElements.js
@@ -4,8 +4,11 @@ function createCheckbox({ array, container }) {
   if (!Array.isArray(array)) throw new TypeError();
   if (!container) throw new Error('No container for your checkboxes defined');
 
-  return array.forEach((item, i) => (
-    container.innerHTML += `<label class="checkbox"><span>${i+1}</span><input type="checkbox" name="${i}" id="${i}"></label>`)
+  return array.forEach(
+    (item, i) =>
+      (container.innerHTML += `<label class="checkbox"><input type="checkbox" name="${i}" id="${i}"><span>${
+        i + 1
+      }</span></label>`)
   );
 }
 
@@ -13,9 +16,9 @@ function createCharacterContainer({ array, container }) {
   if (!Array.isArray(array)) throw new TypeError();
   if (!container) throw new Error('No container for your checkboxes defined');
 
-  return array.forEach((item, i) => (
-    container.innerHTML += `<span data-index="${i}"></span>`
-  ))
+  return array.forEach(
+    (item, i) => (container.innerHTML += `<span data-index="${i}"></span>`)
+  );
 }
 
-export {createCheckbox, createCharacterContainer};
+export { createCheckbox, createCharacterContainer };
